7f5cdf48944a8dad1cc9dc22e7a9244b532d66fe
Gerinaldo Cruz <gerinaldocruz.dev@gmail.com>
2025-10-28 17:25:04 -0300

    feat: adicionando a funcionalidade de remover terefa

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const todosContainer = document.querySelector('.todos-container')
 const creatTodo = taskName => {
    return `<li class="todo-item">
                <span class="task">${taskName}</span>
-               <img src="./images/icons/delete.svg" alt="delete"class="trash">
+               <img src="./images/icons/delete.svg" alt="delete" class="delete">
            </li>`
 }
 
-formAddTask.addEventListener('submit', event => {
+const addTodo = event => {
    event.preventDefault()
 
    const inputValue = event.target.newtask.value.trim()
@@ -18,4 +18,18 @@ formAddTask.addEventListener('submit', event => {
    }
 
    event.target.reset()
-})
+}
+
+const removeTodo = event => {
+   event.stopPropagation()
+
+   const clickedElement = event.target
+   const isDelete = [...clickedElement.classList].includes('delete')
+
+   if (isDelete) {
+      clickedElement.parentElement.remove()
+   }   
+}
+
+formAddTask.addEventListener('submit', addTodo)
+todosContainer.addEventListener('click', removeTodo)
